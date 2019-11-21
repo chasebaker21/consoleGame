51f3bc60a93741568939514b85107b59c794a472
chase <chasebaker21@gmail.com>
2019-11-21 18:18:52 -0500

    cleaned code using methods

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 "use strict";
 
 // created a class for the "good" and "bad" players giving them a name and health
-/**
- * @todo combine the Character and Opponent class since they are the same thing
- * @todo would than need to assign the opponents to a "bad" variable
- */
 class Character {
   constructor(name, health) {
     this.name = name;
@@ -14,112 +10,77 @@ class Character {
   randomDamage = () => Math.floor(Math.random() * 5) + 1;
 
   takeDamage() {
-    this.health -= this.randomDamage();
-  }
-}
-
-class Opponent {
-  constructor(name, health) {
-    this.name = name;
-    this.health = health;
-  }
-  // will damage the opponents randomly between 1 and 10
-  randomDamage = () => Math.floor(Math.random() * 10) + 1;
-
-  takeDamage() {
-    this.health -= this.randomDamage();
+    const damage = this.randomDamage();
+    this.health -= damage;
   }
 }
 
 {
-
-  // set charcters names and healths up
+  // set rebels names and healths up
   let good;
   let bad;
-  let characters = [
-    new Character("Obi Wan", 10),
-    new Character("Luke", 10),
-    new Character("Yoda", 10)
+  let rebels = [
+    new Character("Obi Wan", 100),
+    new Character("Luke", 100),
+    new Character("Yoda", 100)
   ];
-  let opponent = [
-    new Opponent("Darth Vader", 400)
-    // new Opponent("Darth Maul", 300)
+  // sets the empires names and health up
+  let empire = [
+    new Character("Darth Vader", 300)
+    // new empire("Darth Maul", 300)
   ];
-  
+
   /**
    * starts the "game"
-   * uses the classes Character and Opponent
+   * uses the class Character
    * uses for of loop to log through each array
+   * while loop will run while empires health is above 0 and there are more than 1 good players
+   * playerSelect is randomly assigned for each attack against empire
    */
-  // while loop will run while opponents health is above 0
-  // playerSelect is randomly assigned for each attack against opponent
-  while (opponent[0].health >= 0) {
-    let playerSelect = Math.floor(Math.random() * 3) + 1;
-    // loops though the characters array and assigns the to variable good
-    for (good of characters) {
+  while (empire[0].health >= 0 && rebels.length) {
+    let playerSelect = Math.floor(Math.random() * rebels.length) + 1;
+
+    // loops though the rebels array and assigns the to variable good
+    for (good of rebels) {
       console.log(good.name, good.health);
     }
-    // loops though the opponent array (only one at this time) assigns to variable bad
-    for (bad of opponent) {
+
+    // loops though the empire array (only one at this time) assigns to variable bad
+    for (bad of empire) {
       console.log(bad.name, bad.health);
     }
-    
-    // player 1 or "Obi-Wan" will take damage and give damage to opponent and log results
-    if (playerSelect == 1) {
-      characters[0].takeDamage();
-      opponent[0].takeDamage();
-      console.log(characters[0].name, characters[0].health);
-      console.log(opponent[0].name, opponent[0].health);
 
-    // player 2 or "Luke" will take damage and give damage to opponent and log results
-    } else if (playerSelect == 2) {
-      characters[1].takeDamage();
-      opponent[0].takeDamage();
-      console.log(characters[1].name, characters[1].health);
-      console.log(opponent[0].name, opponent[0].health);
+    /**
+     * BELOW IS A GREAT EXAMPLE OF A GOOD USE OF CLASSES/ARRAYS/OBJECTS INSTEAD OF
+     * MULTIPLE IF/ELSE STATEMENTS
+     */
 
-      // player 3 or "Yoda" will take damage and give damage to opponent and log results
-    } else if (playerSelect == 3) {
-      characters[2].takeDamage();
-      opponent[0].takeDamage();
-      console.log(characters[2].name, characters[2].health);
-      console.log(opponent[0].name, opponent[0].health);
-    } 
-
-    // logs to console when the opponent has been defeated game over you win
-    if (opponent[0].health <= 0) {
-      console.log("The force is strong with this one.. You've Won!");
-    }
+    // uses the array selection (playerSelect) to deal damage to that objects health
+    rebels[playerSelect - 1].takeDamage();
+    // uses the array for the empire (one at this time) to deal damage to that objects health
+    empire[0].takeDamage();
+    // logs the results of the good rebels name and health
+    console.log(rebels[playerSelect - 1].name, rebels[playerSelect - 1].health);
+    // logs the results of the empires name and health
+    console.log(empire[0].name, empire[0].health);
 
     /**
      * logs to console if any good character is defeated or all 3 are defeated
      * game over you lose
-     * @todo remove a character if they go below 0 health
-     * @todo characters are now removed but the array is out of order
-     * ^^^^^ need to find the correct way to allow while loop to continue "battle"
      */
-    if (characters[0].health <= 0) {
-      console.log("Obi-Wan has been struck down");
-       characters.splice(0, 1, 0);
-        }
-    if (characters[1].health <= 0) {
-      console.log("Luke has been struck down");
-      characters.splice(1, 1, 0);
-
+    // ANOTHER GOOD EXAMPLE BELOW
+    if (rebels[playerSelect - 1].health <= 0) {
+      console.log(`${rebels[playerSelect - 1].name} has been struck down`);
+      rebels.splice(playerSelect - 1, 1);
     }
-    if (characters[2].health <= 0) {
-      console.log("Yoda has been struck down");
-      characters.splice(2, 1, 0);
-
-    }
-    if (characters.health <= 0) {
+    // logs to the console if the rebels have won, if the empire has won
+    // or if both have fallen
+    if (rebels.length === 0) {
       console.log("You have lost to the dark side.");
+    } else if (empire[0].health <= 0) {
+      console.log("The force is strong with this one.. You've Won!");
+    }else if (rebels.length === 0 && empire[0].health <= 0) {
+      console.log("The Rebels and the Empire has fallen.")
     }
-
   }
 }
-
-
-
-
-
